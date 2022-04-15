27bf6789520a4ba1880ed39661f20a02c59c456e
LAPTOP-UEK22CN8\Irfan <muhammad.bisyr@dataon.com>
2022-04-15 21:25:47 +0700

    New team logo and logo tweak

--- src/asset/team-logo/index.js
+++ src/asset/team-logo/index.js
@@ -7,7 +7,11 @@ import {ReactComponent as Haas} from './haas.svg'
 import {ReactComponent as Mclaren} from './mclaren.svg'
 import {ReactComponent as Mercedes} from './mercedes.svg'
 import {ReactComponent as RedBull} from './red-bull.svg'
+import {ReactComponent as RacingPoint} from './racing-point.svg'
+import {ReactComponent as ForceIndia} from './force-india.svg'
 import {ReactComponent as Williams} from './williams.svg'
+import {ReactComponent as Renault} from './renault.svg'
+import {ReactComponent as Sauber} from './sauber.svg'
 
 export const LOGO_MAP = {
     'red_bull': RedBull,
@@ -20,4 +24,8 @@ export const LOGO_MAP = {
     'williams': Williams,
     'mclaren': Mclaren,
     'aston_martin': AstonMartin,
+    'racing_point': RacingPoint,
+    'force_india': ForceIndia,
+    'renault': Renault,
+    'sauber': Sauber
 }
--- src/component/global/logo/TeamLogo.js
+++ src/component/global/logo/TeamLogo.js
@@ -5,5 +5,5 @@ import { LOGO_MAP } from '../../../asset/team-logo';
 
 export default function TeamLogo({name}){
 
-    return ( <Icon component={LOGO_MAP[name]} style={{ fontSize: '32px', verticalAlign: 'middle', marginTop: '-8px', marginBottom: '-8px'}}></Icon>)
+    return ( <Icon component={LOGO_MAP[name]} style={{ fontSize: '30px', verticalAlign: 'middle', marginTop: '-8px', marginBottom: '-8px'}}></Icon>)
 } 
