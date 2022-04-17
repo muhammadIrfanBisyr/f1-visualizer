2af44a431b520f59538351f8d479a7d14e33f2fc
LAPTOP-UEK22CN8\Irfan <muhammad.bisyr@dataon.com>
2022-04-17 16:42:59 +0700

    update lotus logo

--- src/asset/team-logo/index.js
+++ src/asset/team-logo/index.js
@@ -4,6 +4,7 @@ import {ReactComponent as Alpine} from './alpine.svg'
 import {ReactComponent as AstonMartin} from './aston-martin.svg'
 import {ReactComponent as Ferrari} from './ferrari.svg'
 import {ReactComponent as Haas} from './haas.svg'
+import {ReactComponent as Lotus} from './lotus.svg'
 import {ReactComponent as Mclaren} from './mclaren.svg'
 import {ReactComponent as Mercedes} from './mercedes.svg'
 import {ReactComponent as RedBull} from './red-bull.svg'
@@ -13,7 +14,6 @@ import {ReactComponent as ForceIndia} from './force-india.svg'
 import {ReactComponent as Williams} from './williams.svg'
 import {ReactComponent as Renault} from './renault.svg'
 import {ReactComponent as Sauber} from './sauber.svg'
-import {ReactComponent as Lotus} from './lotus.svg'
 
 export const LOGO_MAP = {
     'red_bull': RedBull,
