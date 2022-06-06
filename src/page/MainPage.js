ecf6b4cf1fd7ab5ca7bde641f6105d89318a5456
Muhammad Irfan Bisyr <irfan100397@gmail.com>
2022-06-06 17:27:01 +0700

    Upgrade antd version

--- src/page/MainPage.js
+++ src/page/MainPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Layout, Typography, Menu } from 'antd';
+import {PieChartOutlined, DesktopOutlined, UserOutlined, TeamOutlined, FileOutlined } from '@ant-design/icons';
 
 import '../style/index.css';
 
@@ -9,6 +10,26 @@ import RaceDetailContextProvider from '../component/race-detail/context/RaceDeta
 const { Title } = Typography;
 const { Sider } = Layout;
 
+function getItem(label, key, icon, children) {
+    return {
+      key,
+      icon,
+      children,
+      label,
+    };
+  }
+
+const items = [
+    getItem('Option 1', '1', <PieChartOutlined />),
+    getItem('Option 2', '2', <DesktopOutlined />),
+    getItem('User', 'sub1', <UserOutlined />, [
+      getItem('Tom', '3'),
+      getItem('Bill', '4'),
+      getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined />),
+  ];
 export default function MainPage(){
 
     return(
@@ -16,7 +37,7 @@ export default function MainPage(){
         <Layout className="main-layout">
             <Sider collapsible>
                 <div className="logo" />
-                <Menu defaultSelectedKeys={['1']} mode="inline" />
+                <Menu defaultSelectedKeys={['1']} mode="inline" items={items}/>
             </Sider>
             <Layout className="content-layout">
                 <>
