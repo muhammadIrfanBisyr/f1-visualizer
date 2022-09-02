bb487e2a179db984b3f34636490234061aeb2dba
Muhammad Irfan Bisyr <irfan100397@gmail.com>
2022-09-02 15:49:39 +0700

    Add leaderboard in season summary

--- src/components/season-summary/Leaderboard.js
+++ src/components/season-summary/Leaderboard.js
@@ -1,19 +1,34 @@
-import React from 'react'
-import { Card, List } from 'antd'
+import React, { useContext } from 'react'
+import { Card, List, Avatar } from 'antd'
 import { CrownFilled } from '@ant-design/icons'
+import SeasonSummaryContext from './context/SeasonSummaryContext'
 
-// const LeaderBoardTop3 = () => {
-//   return (
-//         <></>
-//   )
-// }
+const LeaderBoardTop3 = ({ rank, driverInfo }) => {
+  return (
+        <Card>
+          <CrownFilled></CrownFilled>
+          <Avatar size={50} style={{ marginTop: '16px', border: '3px solid #ffffff' }}/>
+          {
+            driverInfo.driverName
+          }
+        </Card>
+  )
+}
 
 export default function LeaderBoard () {
+  const { dataResults } = useContext(SeasonSummaryContext)
   return (
-    <List>
-        <Card>
-            <CrownFilled></CrownFilled>
-        </Card>
-    </List>
+    <List
+      dataSource={dataResults?.dataSource}
+      renderItem={(item, index) => (
+        <>
+          {
+            index < 3
+              ? <LeaderBoardTop3 rank={index + 1} driverInfo={item}/>
+              : index <= 10 && <Card/>
+          }
+        </>
+      )}
+    />
   )
 }
