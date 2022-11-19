ad641643d3400216bb0c01f930ffa54fe97e916b
Muhammad Irfan Bisyr <irfan100397@gmail.com>
2022-11-19 12:05:54 +0700

    Update driver profile styling

--- src/components/driver-info/driver-detail/DriverDetailProfile.js
+++ src/components/driver-info/driver-detail/DriverDetailProfile.js
@@ -1,14 +1,44 @@
 import React, { useContext } from 'react'
-import { Card, Row, Col, Typography, Space } from 'antd'
+import { Card, Row, Col, Typography } from 'antd'
+
+import DriverAvatar from '../../global/avatar/DriverAvatar'
+import CountryFlag from '../../global/flag/CountryFlag'
 
 import DriverDetailStatistic from './DriverDetailStatistic'
-import DriverAvatar from '../../global/avatar/DriverAvatar'
 import DriverDetailContext from '../context/DriverDetailContext'
 
 const { Text, Title } = Typography
 
+const DriverDetailText = ({ textData = [] }) => {
+  return (
+    <Row>
+    {
+      textData.map((item) => (
+        <>
+          <Col span={10}><Text strong> {item.label}</Text></Col>
+          <Col span={14}>
+            {item.icon}{` ${item.value}`}
+          </Col>
+        </>
+      ))
+    }
+   </Row>
+  )
+}
+
 export default function DriverDetailProfile () {
   const { loading, profileData } = useContext(DriverDetailContext)
+
+  const textData = [
+    {
+      label: 'Nationality',
+      value: profileData.nationality,
+      icon: <CountryFlag nationality={profileData.nationality}></CountryFlag>
+    },
+    { label: 'Date of Birth', value: profileData.dateOfBirth },
+    { label: 'Current Team', value: profileData.permanentNumber },
+    { label: 'Car number', value: profileData.permanentNumber }
+  ]
 
   return (
     <Card className='driver-profile' loading={loading}>
@@ -18,12 +48,7 @@ export default function DriverDetailProfile () {
         </Col>
         <Col span={16} style={{ marginLeft: '16px' }}>
           <Title level={3}>{`${profileData?.givenName} ${profileData?.familyName}`}</Title>
-          <Space direction='vertical' size='small'>
-            <span><Text strong> Nationality</Text> {profileData.nationality}</span>
-            <span><Text strong> Date of Birth</Text> {profileData.dateOfBirth}</span>
-            <span><Text strong> Current Team</Text> {profileData.permanentNumber}</span>
-            <span><Text strong> Car number</Text> {profileData.permanentNumber}</span>
-          </Space>
+          <DriverDetailText textData={textData} />
         </Col>
         <Col span={24}>
           <DriverDetailStatistic/>
